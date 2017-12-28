c91bb9434ec61b6d0119ae682ed38e4790720968
Muhammad Rehman <muhammadrehman@muhammads-mbp.home>
2017-12-28 18:46:48 -0500

    Initial Commit

---
+++ KOok/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KOok
+//
+//  Created by Muhammad Rehman on 12/28/17.
+//  Copyright © 2017 Unit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
