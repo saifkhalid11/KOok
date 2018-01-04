4f8ea19dd7ad2dd33d40cefa3257849be8d1c066
Muhammad Rehman <muhammadrehman@Muhammads-MBP.home>
2018-01-04 09:21:11 -0500

    My first commit

--- KOok/ViewController.swift
+++ KOok/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var Label: UILabel!
+    var tap_count=0
+    @IBAction func Button(_ sender: Any) {
+        /*
+        Label.text="How are you"
+        print("Button tapped")
+ */
+        tap_count=tap_count+1
+        //print(tap_count)
+        if tap_count>=10{
+            Label.text = "You have tapped more than 10 times"
+    }
+    else{
+    print(tap_count)
+    }
+    }
+    
+    @IBOutlet weak var Label2: UILabel!
+    @IBAction func Button2(_ sender: Any) {
+        Label2.text="Buttons are great"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
